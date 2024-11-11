c7b6e38cf98c5de73a8b810def94f6333fb83207
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2024-11-11 11:46:42 +0530

    Origin Changed

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://88nexta-technotes-api.onrender.com',
+    baseUrl: 'https://technotesfor88nextabackend-production.up.railway.app/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
